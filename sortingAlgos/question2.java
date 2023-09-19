451a783f4f59fdc0234271a25ffb99524bc84c7d
Mubassim Ahmed Khan <mubassimkhan@gmail.com>
2023-09-19 20:11:16 +0500

    Changed methods in classes

--- sortingAlgos/question2.java
+++ sortingAlgos/question2.java
@@ -21,6 +21,55 @@ class studentData {
         System.out.println("Course: " + course);
         System.out.println("CGPA: " + cgpa);
     }
+}
+
+public class question2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        studentData[] students = new studentData[2];
+        students[0] = new studentData(34, "Bob", "89", 3.4);
+        students[1] = new studentData(345, "Boba", "890", 37.4);
+
+        // a) Linear search function to display data of a student with a particular Regn no.
+
+        System.out.println("\n Part A \n");
+
+        System.out.print("Enter Register number to find student: ");
+        int regNoToSearch = scanner.nextInt();
+
+        boolean found = false;
+        for (studentData student : students) {
+            if (student != null && student.regNo == regNoToSearch) {
+                student.display();
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Unable to find Student with Reg no: " + regNoToSearch);
+        }
+
+        // b) Bubble sort function to arrange data of students according to Regn no.
+
+        System.out.println("\n Part B \n");
+
+        bubbleSort(students);
+
+        // c) Binary search on the sorted data to display a student with a particular Regn no.
+
+        System.out.println("\n Part C \n");
+
+        System.out.print("Enter Register number to find student data: ");
+        int regNoToBinarySearch = scanner.nextInt();
+        scanner.close();
+        int result = binarySearch(students, regNoToBinarySearch);
+        if (result != -1) {
+            students[result].display();
+        } else {
+            System.out.println("Unable to find Student with Reg no: " + regNoToBinarySearch);
+        }
+    }
 
     // Bubble sort function to arrange data of students according to Regn no.
     static void bubbleSort(studentData[] arr) {
@@ -53,54 +102,5 @@ class studentData {
             }
         }
         return -1; // Student not found
-    }
-
-    public class question2 {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-
-            studentData[] students = new studentData[2];
-            students[0] = new studentData(34, "Bob", "89", 3.4);
-            students[1] = new studentData(345, "Boba", "890", 37.4);
-
-            // a) Linear search function to display data of a student with a particular Regn no.
-
-            System.out.println("\n Part A \n");
-
-            System.out.print("Enter Register number to find student: ");
-            int regNoToSearch = scanner.nextInt();
-
-            boolean found = false;
-            for (studentData student : students) {
-                if (student != null && student.regNo == regNoToSearch) {
-                    student.display();
-                    found = true;
-                }
-            }
-
-            if (!found) {
-                System.out.println("Unable to find Student with Reg no: " + regNoToSearch);
-            }
-
-            // b) Bubble sort function to arrange data of students according to Regn no.
-            
-            System.out.println("\n Part B \n");
-
-            bubbleSort(students);
-
-            // c) Binary search on the sorted data to display a student with a particular Regn no.
-
-            System.out.println("\n Part C \n");
-
-            System.out.print("Enter Register number to find student data: ");
-            int regNoToBinarySearch = scanner.nextInt();
-            scanner.close();
-            int result = binarySearch(students, regNoToBinarySearch);
-            if (result != -1) {
-                students[result].display();
-            } else {
-                System.out.println("Unable to find Student with Reg no: " + regNoToBinarySearch);
-            }
-        }
     }
 }
